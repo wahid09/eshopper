51054f877f630fa5d21b22b901b2261a0488108e
wahid09 <wahid.ullah.cse@gmail.com>
2018-11-23 17:11:08 +0600

    Product add

---
+++ admin/productedit.php
@@ -0,0 +1,171 @@
+<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+<?php include '../classes/Product.php'; ?>
+<?php include '../classes/Brand.php'; ?>
+<?php include '../classes/Ctegory.php'; ?>
+
+<?php
+if (!isset($_GET['pdid']) || $_GET['pdid'] == NULL) {
+    echo "<script>window.location='productlist.php'; </script>";
+}else{
+    $id = $_GET['pdid'];
+    $pattern = '/^-a-zA-Z0-9_/';
+    $replacement = '';
+    $id = preg_replace($pattern, $replacement, $id);
+}
+
+$pd = new Product();
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+ $updateProduct = $pd->productUpdate($_POST, $_FILES, $id);
+}
+?>
+
+<div class="grid_10">
+    <div class="box round first grid">
+        <h2>Add New Product</h2>
+        <div class="block">
+        <?php 
+        if (isset($updateProduct)) {
+            echo $updateProduct;
+        }
+         ?>
+         <?php
+         $getpd = $pd->getProductById($id);
+         if ($getpd) {
+             while ($value = $getpd->fetch_assoc()) {
+
+         ?>           
+         <form action="" method="post" enctype="multipart/form-data">
+            <table class="form">
+               
+                <tr>
+                    <td>
+                        <label>Name</label>
+                    </td>
+                    <td>
+                        <input type="text" name="productName" value="<?php echo $value['productName']; ?>" class="medium" />
+                    </td>
+                </tr>
+				<tr>
+                    <td>
+                        <label>Category</label>
+                    </td>
+                    <td>
+                        <select id="select" name="catId">
+                            <option>Select Category</option>
+                            <?php
+                            $cat = new Category();
+                            $getCat = $cat->getAllcat();
+                            if ($getCat) {
+                                while ($result = $getCat->fetch_assoc()) {
+                            ?>
+                            <option
+                            <?php
+                            if ($value['catId'] == $result['catId']) { ?>
+                                selected = "selected";
+                            <?php }
+                            ?>
+                           value="<?php echo $result['catId']; ?>"><?php echo $result['catName']; ?></option>
+                            <?php  }
+                            } ?>
+                        </select>
+                    </td>
+                </tr>
+				<tr>
+                    <td>
+                        <label>Brand</label>
+                    </td>
+                    <td>
+                        <select id="select" name="brandId">
+                            <option>Select Brand</option>
+                            <?php 
+                            $brand = new Brand();
+                            $getBrand = $brand->getAllBrand();
+                            if ($getBrand) {
+                                while ($result = $getBrand->fetch_assoc()) {  
+                            ?>
+                            <option
+                            <?php 
+                            if($value['brandId'] == $result['brandId']){ ?>
+                                selected = "selected";
+                            <?php }
+                            ?> 
+                            value="<?php echo $result['brandId'] ?>"><?php echo $result['brandName'] ?></option>
+                        <?php } } ?>
+                        </select>
+                    </td>
+                </tr>
+				
+				 <tr>
+                    <td style="vertical-align: top; padding-top: 9px;">
+                        <label>Description</label>
+                    </td>
+                    <td>
+                        <textarea class="tinymce" name="body">
+                            <?php echo $value['body']; ?>
+                        </textarea>
+                    </td>
+                </tr>
+				<tr>
+                    <td>
+                        <label>Price</label>
+                    </td>
+                    <td>
+                        <input type="text" name="price" value="<?php echo $value['price']; ?>" class="medium" />
+                    </td>
+                </tr>
+            
+                <tr>
+                    <td>
+                        <label>Upload Image</label>
+                    </td>
+                    <td>
+                        <img src="<?php echo $value['image']; ?>" width= "100px" height="100px"><br/>
+                        <input type="file" name="image" />
+                    </td>
+                </tr>
+				
+				<tr>
+                    <td>
+                        <label>Product Type</label>
+                    </td>
+                    <td>
+                        <select id="select" name="type">
+                            <option>Select Type</option>
+                            <?php 
+                            if ($value['type'] == 0) { ?>
+                            <option selected = "selected"; value="0">Featured</option>
+                            <option value="1">General</option>
+                            <?php }else{ ?>
+                            <option selected = "selected"; value="1">General</option>
+                            <option value="0">Featured</option>
+                            <?php } } ?>
+                        </select>
+                    </td>
+                </tr>
+
+				<tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" name="submit" Value="Save" />
+                    </td>
+                </tr>
+            </table>
+            </form>
+            <?php }  ?>
+        </div>
+    </div>
+</div>
+<!-- Load TinyMCE -->
+<script src="js/tiny-mce/jquery.tinymce.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        setupTinyMCE();
+        setDatePicker('date-picker');
+        $('input[type="checkbox"]').fancybutton();
+        $('input[type="radio"]').fancybutton();
+    });
+</script>
+<!-- Load TinyMCE -->
+<?php include 'inc/footer.php'; ?>
--- classes/Product.php
+++ classes/Product.php
@@ -32,10 +32,15 @@ class Product
 		$unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
 		$uploaded_image = "upload/".$unique_image;
 
-		if ($productName =="" || $catId =="" || $brandId =="" || $body =="" || $price =="" || $type =="") {
+		if ($productName =="" || $catId =="" || $brandId =="" || $body =="" || $price =="" || $file_name =="" || $type =="") {
 			$msg ="<span class='error'>Filds must not be empty</span>";
 			return $msg;
-		}else{
+		}elseif ($file_size >1048567) {
+         echo "<span class='error'>Image Size should be less then 1MB!
+         </span>";
+         }elseif (in_array($file_ext, $permited) === false) {
+          echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
+          }else{
 			move_uploaded_file($file_temp, $uploaded_image);
 			$query = "INSERT INTO tbl_product(productName, catId, brandId, body, price, image, type) VALUES('$productName', '$catId', '$brandId', '$body', '$price', '$uploaded_image', '$type')";
 			$productInsert = $this->db->insert($query);
@@ -46,6 +51,112 @@ class Product
 				$msg = "<span class='error'>Product not inserted</span>";
 				return $msg;
 			}
+		}
+	}
+	public function getAllProduct(){
+		$query = "SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName
+		         FROM tbl_product
+		         INNER JOIN tbl_category
+		         ON tbl_product.catId = tbl_category.catId
+		         INNER JOIN tbl_brand
+		         ON tbl_product.brandId = tbl_brand.brandId
+		         ORDER BY tbl_product.productId DESC";
+		$result = $this->db->select($query);
+		return $result;
+	}
+	public function getProductById($id){
+		$query = "SELECT * FROM tbl_product WHERE productId = '$id'";
+		$result = $this->db->select($query);
+		return $result;
+	}
+	public function productUpdate($data, $file, $id){
+		$productName = mysqli_real_escape_string($this->db->link, $data['productName']);
+		$catId       = mysqli_real_escape_string($this->db->link, $data['catId']);
+		$brandId     = mysqli_real_escape_string($this->db->link, $data['brandId']);
+		$body        = mysqli_real_escape_string($this->db->link, $data['body']);
+		$price       = mysqli_real_escape_string($this->db->link, $data['price']);
+		$type        = mysqli_real_escape_string($this->db->link, $data['type']);
+		//image file
+		$permited  = array('jpg', 'jpeg', 'png', 'gif');
+		$file_name = $file['image']['name'];
+		$file_size = $file['image']['size'];
+		$file_temp = $file['image']['tmp_name'];
+		$div = explode('.', $file_name);
+		$file_ext = strtolower(end($div));
+		$unique_image = substr(md5(time()), 0, 10).'.'.$file_ext;
+		$uploaded_image = "upload/".$unique_image;
+
+		if ($productName =="" || $catId =="" || $brandId =="" || $body =="" || $price =="" || $type =="") {
+			$msg ="<span class='error'>Filds must not be empty</span>";
+			return $msg;
+		}else{
+			if(!empty($file_name)){
+		if ($file_size >1048567) {
+         echo "<span class='error'>Image Size should be less then 1MB!
+         </span>";
+         }elseif (in_array($file_ext, $permited) === false) {
+          echo "<span class='error'>You can upload only:-".implode(', ', $permited)."</span>";
+          } else{
+			move_uploaded_file($file_temp, $uploaded_image);
+			$query = "UPDATE tbl_product
+			          SET
+			          productName = '$productName',
+			          catId       = '$catId',
+			          brandId     = '$brandId', 
+			          body        = '$body',
+			          price       = '$price',
+			          image       = '$uploaded_image',
+			          type        = '$type'
+			          WHERE productId = '$id'";
+			$productUpdate = $this->db->update($query);
+			if ($productUpdate) {
+				$msg = "<span class='success'>Product update successfully</span>";
+				return $msg;
+			}else{
+				$msg = "<span class='error'>Product not updated</span>";
+				return $msg;
+			}
+		}
+	}else{
+		$query = "UPDATE tbl_product
+			          SET
+			          productName = '$productName',
+			          catId       = '$catId',
+			          brandId     = '$brandId', 
+			          body        = '$body',
+			          price       = '$price',
+			          type        = '$type'
+			          WHERE productId = '$id'";
+			$productUpdate = $this->db->update($query);
+			if ($productUpdate) {
+				$msg = "<span class='success'>Product update successfully</span>";
+				return $msg;
+			}else{
+				$msg = "<span class='error'>Product not updated</span>";
+				return $msg;
+			}
+
+	}
+	}
+
+	}
+	public function deleteProducrById($id){
+		$query = "SELECT * FROM tbl_product WHERE productId = '$id'";
+		$getData = $this->db->select($query);
+		if ($getData) {
+			while ($value = $getData->fetch_assoc()) {
+				$delImage = $value['image'];
+				unlink($delImage);
+			}
+		}
+		$delquery = "DELETE FROM tbl_product WHERE productId = '$id'";
+		$delData = $this->db->delete($delquery);
+		if ($delData) {
+			$msg = "<span class='success'>Product delete successfully</span>";
+				return $msg;
+		}else{
+			$msg = "<span class='error'>Product not deleteed</span>";
+				return $msg;
 		}
 	}
 }
